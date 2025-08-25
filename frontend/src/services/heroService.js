8858e8cddd0e0c9346a8aa6dc135bf5c4d5274a5
C1998-ops <ckumargn1998@gmail.com>
2025-08-25 14:38:07 +0530

    Fix: Correct backend url (/api is missing) heroService.jsx

--- frontend/src/services/heroService.js
+++ frontend/src/services/heroService.js
@@ -3,11 +3,11 @@ import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export const getHeroContent = async () => {
-  const response = await axios.get(`${apiUrl}/api/hero-content`);
+  const response = await axios.get(`${apiUrl}/api/hero/hero-content`);
   return response.data;
 };
 
 export const updateHeroContent = async (content) => {
-  const response = await axios.put(`${apiUrl}/api/hero-content-update`, content);
+  const response = await axios.put(`${apiUrl}/api/hero/hero-content-update`, content);
   return response.data;
 };
